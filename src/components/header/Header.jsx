bbd7507bd91223ddf124ad45c4f762f3c6655e95
Yeliazar <erik.ruzhakov@mail.ru>
2022-07-30 17:08:38 +0300

    add components CTA, social header buttons, add images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import "./header.css";
+import CTA from './CTA/CTA';
+import ME from '../../assets/m3.jpg';
+import HeaderSocial from "./HeaderSocials/HeaderSocials";
+
 
 function Header() {
     return (
-        <div>Test</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello? I'm</h5>
+                <h1>Yeliazar Kazhura</h1>
+                <h5 className="text-light">Fullstack developer</h5>
+                <CTA/>
+                <HeaderSocial/>
+                
+                <div className="me">
+                    <img src={ME} alt="me"/>
+                </div>
+
+                <a href="#contact" className="scroll_down">Scroll Down</a>
+            </div>
+        </header>
     );
 }
 
